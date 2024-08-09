ed772e3e92c6a770fb08f83500c05cb671a468bd
Eduardo Diogo <educdiogo@hotmail.com>
2024-08-08 21:58:23 -0400

    chore: react-router-dom config

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CreateRoom />,
+  },
+  {
+    path: '/room/:id',
+    element: <Room />,
+  }
+])
+
 export function App() {
-  return <h1>Hello world</h1>
+  return <RouterProvider router={Router} />
 }
