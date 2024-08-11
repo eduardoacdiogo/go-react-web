30815b02adf0f2084fe1e574433b1dfe63951350
Eduardo Diogo <educdiogo@hotmail.com>
2024-08-11 15:00:43 -0400

    feature: room page UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'sonner'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
@@ -14,5 +15,10 @@ const Router = createBrowserRouter([
 ])
 
 export function App() {
-  return <RouterProvider router={Router} />
+  return (
+    <>
+      <RouterProvider router={Router} />
+      <Toaster invert richColors />
+    </>
+  )
 }
