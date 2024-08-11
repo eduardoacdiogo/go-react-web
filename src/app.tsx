a02a9f60f73013e2b3a00f7a29659ade19dd982a
Eduardo Diogo <educdiogo@hotmail.com>
2024-08-11 18:37:53 -0400

    chore: Query Client config

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,8 @@ import { Toaster } from 'sonner'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 const Router = createBrowserRouter([
   {
@@ -16,9 +18,9 @@ const Router = createBrowserRouter([
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={Router} />
       <Toaster invert richColors />
-    </>
+    </QueryClientProvider>
   )
 }
